d2ec1518fef70a46050840622ddfc1a4c4bde293
Gui7F <gui643216@gmail.com>
2024-02-29 00:44:42 -0300

    PropsTypes, definição de tipos e validação

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,31 @@
-function Item(props){
+// Podemos definir o tipo de dado com o pacote PropTypes do react, podemos dizer neste exemplo
+//que ano_lançamento é um number e marca é uma string isso pode servir para proteger o programador
+// de enviar dados de tipo diferentes para as propriedades, pois o erro aparace no console do navegador
+
+import PropTypes from "prop-types"
+
+function Item({marca, ano_lançamento}){
     return(
         <>
-         <li>{props.marca}</li>
+         <li>{marca}, {ano_lançamento}</li>
         </>
     )
 }
+
+Item.propTypes = {
+    marca : PropTypes.string,
+    ano_lançamento: PropTypes.number
+}
+
+// Aqui estamos enviando o tipo de dado para cada chave do objeto oque ajuda bastante tambem na validação a nivel de programadores, que futuramente
+// mexeram no seu codigo.
+
+Item.defaultProps = {
+    marca : "Faltou Marca", 
+    ano_lançamento: "Faltou Ano"
+}
+
+// Aqui tambem estamos colocando outra validação que serve apenas para programadores, que é um valor padrão para o item caso ele seja 
+// colocado de forma vazia na lista.
 
 export default Item
--- src/components/List.js
+++ src/components/List.js
@@ -5,9 +5,10 @@ function List(){
         <>
           <h1>Minha Lista</h1>
           <ul>
-            <Item marca = "Ferrari" />
-            <Item marca = "Fiat" />
-            <Item marca = "Lamborghine" />
+            <Item marca = "Ferrari"  ano_lançamento={1985}/>
+            <Item marca = "Fiat" ano_lançamento={1964} />
+            <Item marca = "Renault" ano_lançamento={1999}/>
+            <Item></Item> 
           </ul>
         </>
     )
